d1c2b86e5ea742298ed286d60569c670c638c7f9
DevJo8 <152252543+DevJo8@users.noreply.github.com>
2025-07-25 22:10:31 +0700

    Update page.tsx

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -107,7 +107,7 @@ export default function ChatPage() {
   const { connection } = useConnection();
   const [balance, setBalance] = useState<number | null>(null); // State to hold the balance
 
-const contractAddress = "aksadhgshdywfdywfdyw";
+const contractAddress = "COMING SOON";
 const [copied, setCopied] = useState(false);
 const [copiedWallet, setCopiedWallet] = useState(false); // For wallet address copy
 const handleCopy = () => {
